a3455e856f6e8ea3af9bd2729d7a5bdf010e18b1
James Mooring <james@lioneagle.solutions>
2025-05-01 09:16:53 +1000

    Revert "update deps"
    This reverts commit 59ab3dc7c4ab51b7bfb7b4ba02fa915863a5a964.

---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Document Tracker"
+  actions:
+    - theme: brand
+      text: Getting Started
+      link: /getting-started
+---
--- src/client.ts
+++ src/client.ts
@@ -6,7 +6,6 @@ const api = axios.create({
     Accept: "application/json",
   },
   withCredentials: true,
-  withXSRFToken: true,
 });
 
 export type LoginPayload = {
@@ -25,9 +24,8 @@ type Document = {
   expires_at: string;
 };
 export const documentsClient = {
-  getDocuments: () => api.get<{ data: Document[] }>("/api/documents"),
-  getDocument: (id: number) =>
-    api.get<{ data: Document }>(`/api/documents/${id}`),
-  archiveDocument: (id: number) => api.post(`/api/documents/${id}/archive`),
-  createDocument: (id: number) => api.post(`/api/documents/${id}`),
+  getDocuments: () => api.get<{ data: Document[] }>("/documents"),
+  getDocument: (id: number) => api.get<{ data: Document }>(`/documents/${id}`),
+  archiveDocument: (id: number) => api.post(`/documents/${id}/archive`),
+  createDocument: (id: number) => api.post(`/documents/${id}`),
 };
